60d47a68b06436da461df777089f1c9209fedb04
adrien-dubois <mcsnoos@gmail.com>
2022-04-23 23:23:37 +0200

    [ADD]Update post endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import {
   getPosts,
-  createPost
+  createPost,
+  updatePost
 } from '../controllers/posts.js';
 
 const router = express.Router();
@@ -9,5 +10,6 @@ const router = express.Router();
 // http://localhost:5000/posts
 router.get('/', getPosts);
 router.post('/', createPost);
+router.patch('/:id', updatePost);
 
 export default router;
