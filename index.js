89d8838cf778c17bcdd6b522a17986fa88c8c41e
adrien-dubois <mcsnoos@gmail.com>
2022-04-24 21:46:47 +0200

    [EDIT]Lighter code

--- index.js
+++ index.js
@@ -14,9 +14,8 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.sawlf.mongodb.net/whiteRabbit?retryWrites=true&w=majority`;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.DB_CONNECT)
   .then(() => app.listen(PORT, () => console.log(`Connected to White Rabbit Server on port ${PORT}`)))
   .catch((error) => console.log(error))
